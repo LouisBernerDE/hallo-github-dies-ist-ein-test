591b29d4b8eb6ced0a6b9e68792e4fde3e072bf8
Louis Berner <l.berner@msoft.de>
2023-06-23 14:56:18 +0200

    c

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -8,13 +8,21 @@ namespace Konsolenanwendung_Tutorial
         static void Main(string[] args)
         {
             ///FG & BG Color sind nur für die Schrift, nicht den eigentlichen Hintergrund der Anzeige.
-            Console.ForegroundColor = ConsoleColor.Yellow; 
-            Console.BackgroundColor= ConsoleColor.Magenta;
-          ///Hier steht, was ausgegeben wird wenn die Anwendung läuft.
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            ///Hier steht, was ausgegeben wird wenn die Anwendung läuft.
             Console.WriteLine("Hello World!");
 
-            ///Zeile eingefügt damit das Fenster offen bleibt und tab benötig wird zum beenden.
-            Console.ReadKey();
+            ///Zeile eingefügt damit das Fenster offen bleibt und Tab benötig wird zum beenden.
+            while (true)
+            {
+                ConsoleKeyInfo test = Console.ReadKey();
+                if (test.Key == ConsoleKey.Tab)
+
+                    break;
+
+
+            }
         }
     }
 }
